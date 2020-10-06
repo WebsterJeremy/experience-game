f5f8963acc721dc893a44abaf414208eec9dfc22
MatthewMaddern1 <matt.maddern@flinders.edu.au>
2020-10-06 18:27:41 +1030

    add basic cross hair and gun icon on HUD

--- ExperienceGame/Assets/Scripts/UI/HUD.cs
+++ ExperienceGame/Assets/Scripts/UI/HUD.cs
@@ -9,18 +9,23 @@ public class HUD : MonoBehaviour
 {
 
     // modify the text objects via the Update...() functions below
-    private TMP_Text textAmmo, textHealth;
+    private TMP_Text textAmmo, textHealth, textGun;
+    private Image gunIcon;
 
     // modify via SetHUDPosition
-    private RectTransform textAmmoRectTransform, textHealthRectTransform;
+    private RectTransform textAmmoRectTransform, textHealthRectTransform, textGunRectTransform, gunIconRectTransform;
 
     public HUD()
     {
         // text mesh pro objects
+        gunIcon    = GameObject.Find("img_gun_icon").GetComponentInChildren<Image>();
+        textGun    = GameObject.Find("text_gun").GetComponentInChildren<TMP_Text>();
         textAmmo   = GameObject.Find("text_ammo").GetComponentInChildren<TMP_Text>();
         textHealth = GameObject.Find("text_health").GetComponentInChildren<TMP_Text>();
 
         // the rect transform of the text mesh pro objects - used for positioning
+        gunIconRectTransform    = gunIcon.GetComponent<RectTransform>();
+        textGunRectTransform    = textGun.GetComponent<RectTransform>();
         textAmmoRectTransform   = textAmmo.GetComponent<RectTransform>();
         textHealthRectTransform = textHealth.GetComponent<RectTransform>();
     }
@@ -50,11 +55,14 @@ public class HUD : MonoBehaviour
         float newTextX = -(newWidth / 2) + textAmmo.preferredWidth;
 
         // calculate the new Y position for each element
-        float newAmmoTextY   = (newHeight / 2) - textAmmo.preferredHeight * 2;
-        float newHealthTextY = newAmmoTextY - textHealth.preferredHeight;
+        float newHealthTextY = (newHeight / 2) - textAmmo.preferredHeight * 2;
+        float newAmmoTextY   = newHealthTextY  - textHealth.preferredHeight;
+        float newGunthTextY  = newAmmoTextY    - textGun.preferredHeight;
 
         // set the new position of the elements
         textAmmoRectTransform.anchoredPosition   = new Vector3(newTextX, newAmmoTextY,   0);
         textHealthRectTransform.anchoredPosition = new Vector3(newTextX, newHealthTextY, 0);
+        textGunRectTransform.anchoredPosition    = new Vector3(newTextX, newGunthTextY,  0);
+        gunIconRectTransform.anchoredPosition    = new Vector3(newTextX + textGun.preferredWidth, newGunthTextY, 0);
     }
 }
